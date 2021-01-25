fffd972946bfbad558071833a47e8d7ca5368750
Maku1007 <ts.manlai1007@gmail.com>
2021-01-25 20:51:16 +0800

    state bolon search controller nemne

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,35 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+/**
+ * Web app төлөв
+ * - Хайлтын query, үр дүн
+ * - Тухайн үзүүлж байгаа жор
+ * - Лайкласан жорууд
+ * - Захиалж байгаа жорын найрлагууд
+ */
 
-search.doSearch().then((r) => console.log(r));
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Вебээс хайлтын түлхүүр үгийг гаргаж авна.
+  const query = "pizza";
+
+  if (query) {
+    // 2) Шинээр хайлтын объектыг үүсгэж өгнө.
+    state.search = new Search(query);
+
+    // 3) Хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ.
+
+    // 4) Хайлтыг гүйцэтгэнэ.
+    await state.search.doSearch();
+
+    // 5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
